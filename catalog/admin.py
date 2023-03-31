68e354860c805959451f3d217867c7a036a1803c
Kratos1top <strong.willed343@gmail.com>
2023-03-31 14:59:54 -0500

    Created borrowed field to book instance

--- catalog/admin.py
+++ catalog/admin.py
@@ -11,16 +11,17 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
 
 
+@admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('status', 'due_back')
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
         (None, {
-            'fields': ('book', 'imprint', 'id')
+            'fields': ('book','imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back','borrower')
         }),
     )
 
@@ -28,4 +29,3 @@ class BookInstanceAdmin(admin.ModelAdmin):
 admin.site.register(Genre)
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book, BookAdmin)
-admin.site.register(BookInstance, BookInstanceAdmin)
--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,16 @@ from django.http import Http404
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views.generic import DetailView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, ListView):
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
 
 
 def index(request):
@@ -71,5 +81,3 @@ class AuthorDetailView(DetailView):
             'author_detail.html',
             context={'author': author_id}
         )
-
-
